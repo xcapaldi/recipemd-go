3471d8585d24afaecd4b5b53be356c3d064ee958
Xavier Capaldi <38892330+xcapaldi@users.noreply.github.com>
2026-03-13 11:55:50 -0400

    fix: refactor rendering logic
    Clean it up and split into files.

---
+++ render_markdown.go
@@ -0,0 +1,67 @@
+package recipemd
+
+import (
+	"bytes"
+	"strings"
+	"text/template"
+)
+
+type mdGroupCtx struct {
+	IngredientGroup
+	Level int
+}
+
+func (g mdGroupCtx) Heading() string {
+	return strings.Repeat("#", g.Level)
+}
+
+func (g mdGroupCtx) Subgroups() []mdGroupCtx {
+	out := make([]mdGroupCtx, len(g.IngredientGroups))
+	for i, sg := range g.IngredientGroups {
+		out[i] = mdGroupCtx{sg, g.Level + 1}
+	}
+	return out
+}
+
+const mdMainTmpl = `# {{ .Title }}
+{{ with deref .Description }}
+{{ . }}
+{{ end }}{{ if .Tags }}
+*{{ join .Tags ", " }}*
+{{ end }}{{ if .Yields }}
+**{{ serializeYields .Yields }}**
+{{ end }}
+---
+{{ template "ingredients" .Ingredients }}{{ template "groups" (topGroups .IngredientGroups) }}{{ with deref .Instructions }}
+---
+
+{{ . }}
+{{ end }}`
+
+const mdIngredientsTmpl = `{{ if . }}
+{{ range . }}- {{ if .Amount }}*{{ serializeAmount .Amount }}* {{ end }}{{ if .Link }}[{{ .Name }}]({{ deref .Link }}){{ else }}{{ .Name }}{{ end }}
+{{ end }}{{ end }}`
+
+const mdGroupsTmpl = `{{ range . }}
+{{ .Heading }} {{ .Title }}
+{{ template "ingredients" .Ingredients }}{{ template "groups" (.Subgroups) }}{{ end }}`
+
+// RenderMarkdown formats a Recipe as RecipeMD markdown.
+func (p *Parser) RenderMarkdown(r *Recipe, rounding int) string {
+	funcs := renderFuncMap(rounding)
+	funcs["topGroups"] = func(groups []IngredientGroup) []mdGroupCtx {
+		out := make([]mdGroupCtx, len(groups))
+		for i, g := range groups {
+			out[i] = mdGroupCtx{g, 2}
+		}
+		return out
+	}
+
+	tmpl := template.Must(template.New("recipemd").Funcs(funcs).Parse(mdMainTmpl))
+	template.Must(tmpl.New("ingredients").Parse(mdIngredientsTmpl))
+	template.Must(tmpl.New("groups").Parse(mdGroupsTmpl))
+
+	var buf bytes.Buffer
+	_ = tmpl.Execute(&buf, r)
+	return buf.String()
+}
